5fc398a3777a9063f7dcc51a3f8453d18252814b
vishnu <vishnu.nair@datoms.io>
2022-03-07 00:19:17 +0530

    new application; add code

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,21 +1,12 @@
-package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class Application {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+package com.huongdanjava.springboot;
+ 
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class Calculation {
+ 
+  public int sum(int a, int b) {
+    return a + b;
+  }
 }
